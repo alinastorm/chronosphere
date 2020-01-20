0e78d29c167e5a5112fb4931c3e22d0854855370
Alex Guriev <sasaguryev@mail.ru>
2020-01-20 21:29:25 +0300

    +style (map opacity transition)

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -13,13 +13,15 @@ export class CardsComponent implements OnInit {
 
 
   public models;
-
-
+  opacity = false;
+  opacityWithPause = false;
   constructor(private _modelsService: ModelsService) {
 
   }
 
   ngOnInit(): void {
+
+    setTimeout(() => this.opacityWithPause = true, 100);
     this._modelsService.getModels().pipe(
       map(models => {
         for (const iterator of models.data) {
@@ -34,5 +36,7 @@ export class CardsComponent implements OnInit {
       );
 
   }
+
+
 
 }
